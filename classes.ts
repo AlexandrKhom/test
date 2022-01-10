cc5990b8dc11b5530a5a548963870bc585208258
oleksandr.khomenko <oleksandr.khomenko@leverx.com>
2022-01-10 18:14:39 +0200

    classes2

--- classes.ts
+++ classes.ts
@@ -21,14 +21,44 @@ class Car1 {
     constructor(readonly model: string) {}
 }
 
+// =========
 
+class Animal {
+    public color: string = 'black'
+    protected voice: string = ''
+    constructor() {
+        console.log('go-go-go')
+    }
+    private  go() {
+        console.log('GO')
+    }
+}
 
+class Cat extends Animal {
+    public setVoice(voice: string): void {
+        this.voice = voice
+    }
+}
 
+const cat = new Cat()
+cat.color = 'red'
+cat.setVoice('ko-ko')
+//cat.voice  //err
 
+//===== abstract class =====
 
+abstract class Component {
+    abstract render(): void
+    abstract info(): string
+}
 
-
-
-
+class AppComponent extends Component {
+    render(): void {
+        console.log('void log')
+    }
+    info(): string {
+        return 'this is info'
+    }
+}
 
 
